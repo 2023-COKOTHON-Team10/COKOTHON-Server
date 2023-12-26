e545ce1c8972b9e7973b2c24252c01133a5d5614
sangkim99 <rettsadd990922@kookmin.ac.kr>
2023-12-26 18:50:21 +0900

    dependency 추가

---
+++ src/main/java/com/example/team10/user/domain/User.java
@@ -0,0 +1,14 @@
+package com.example.team10.user.domain;
+
+
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Table
+
+public class User {
+
+}
