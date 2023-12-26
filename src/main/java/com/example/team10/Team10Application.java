219ec32302b47192cd37074e81f1bd24c6670420
pysdsc <pysdsc@gmail.com>
2023-12-26 17:19:28 +0900

    test

--- src/main/java/com/example/team10/Team10Application.java
+++ src/main/java/com/example/team10/Team10Application.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Team10Application {
 
+    // test
     public static void main(String[] args) {
         SpringApplication.run(Team10Application.class, args);
     }
+
 
 }
