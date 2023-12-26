06b739bc648029c11e63bd74e33d3a765aea1883
sangkim99 <rettsadd990922@kookmin.ac.kr>
2023-12-26 21:42:04 +0900

    4번 페이지까지 기능 완성

--- src/main/java/com/example/team10/user/UserController.java
+++ src/main/java/com/example/team10/user/UserController.java
@@ -2,9 +2,7 @@ package com.example.team10.user;
 
 import com.example.team10.user.dto.UserCreateRequest;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
@@ -13,7 +11,12 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/api/user")
-    public void saveUser(@RequestBody UserCreateRequest request){
-        userService.saveUser(request);
+    public Long saveUser(@RequestBody UserCreateRequest request){
+        return userService.saveUser(request);
+    }
+
+    @GetMapping("/api/getUniqueNum")
+    public String getUniqueNum(@RequestParam Long user_id) {
+        return userService.showUniqueNum(user_id);
     }
 }
--- src/main/java/com/example/team10/user/UserService.java
+++ src/main/java/com/example/team10/user/UserService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.aventrix.jnanoid.jnanoid.NanoIdUtils;
+
+import java.util.Optional;
 import java.util.Random;
 
 @Service
@@ -18,14 +20,21 @@ public class UserService {
     private final char[] numbers = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
     @Transactional
-    public void saveUser(UserCreateRequest request){
+    public Long saveUser(UserCreateRequest request){
         User user = User.builder()
                 .name(request.getName())
                 .uniqueNumber(newNanoID())
                 .build();
         userRepository.save(user);
+        return user.getId();
     }
 
     public String newNanoID() {return NanoIdUtils.randomNanoId(random, numbers, 6);}
+
+    @Transactional
+    public String showUniqueNum(Long userId){
+        Optional<User> user = userRepository.findById(userId);
+        return user.get().getUniqueNumber();
+    }
 
 }
