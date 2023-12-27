ce619335d17455a3759a38a9d8a898a9ae7cf849
sangkim99 <rettsadd990922@kookmin.ac.kr>
2023-12-27 11:45:53 +0900

    고유번호 검색 param -> requset

--- src/main/java/com/example/team10/answer/AnswerController.java
+++ src/main/java/com/example/team10/answer/AnswerController.java
@@ -17,5 +17,4 @@ public class AnswerController {
         answerService.save(request);
     }
 
-
 }
--- src/main/java/com/example/team10/user/UserController.java
+++ src/main/java/com/example/team10/user/UserController.java
@@ -1,6 +1,7 @@
 package com.example.team10.user;
 
 import com.example.team10.user.dto.UserCreateRequest;
+import com.example.team10.user.dto.UserGetRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,7 +17,7 @@ public class UserController {
     }
 
     @GetMapping("/api/getUniqueNum")
-    public String getUniqueNum(@RequestParam Long user_id) {
-        return userService.showUniqueNum(user_id);
+    public String getUniqueNum(@RequestBody UserGetRequest request) {
+        return userService.showUniqueNum(request.getUser_id());
     }
 }
